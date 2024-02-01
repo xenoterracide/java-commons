33f17b4b3571feb252a5e2ab4d7a8ed3d0f9d537
Caleb Cushing <xenoterracide@gmail.com>
2024-02-01 02:04:16 -0500

    chore: test, test app (#7)

--- module/test-app/build.gradle.kts
+++ module/test-app/build.gradle.kts
@@ -8,6 +8,13 @@ dependencies {
   implementation(platform(libs.spring.bom))
   implementation(libs.spring.boot.autoconfigure)
 
+  testRuntimeOnly(platform(libs.spring.bom))
+  testRuntimeOnly(libs.bundles.junit.platform)
+  testRuntimeOnly(libs.bundles.spring.test)
+
+  testImplementation(platform(libs.spring.bom))
+  testImplementation(libs.junit.api)
+  testImplementation(libs.spring.boot.test.autoconfigure)
   modules {
     module("org.springframework.boot:spring-boot-starter-logging") {
       replacedBy(
