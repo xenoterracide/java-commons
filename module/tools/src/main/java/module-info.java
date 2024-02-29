35758ae226b7e54ebf357dae375b06f5c57f1a58
Caleb Cushing <xenoterracide@gmail.com>
2024-02-29 10:18:20 -0500

    chore: nullmark module too

--- module/tools/src/main/java/module-info.java
+++ module/tools/src/main/java/module-info.java
@@ -1,4 +1,6 @@
-module com.xenoterracide.tools.java {
+import org.jspecify.annotations.NullMarked;
+
+@NullMarked module com.xenoterracide.tools.java {
   requires java.base;
   requires org.jspecify;
   exports com.xenoterracide.tools0.java.function;
