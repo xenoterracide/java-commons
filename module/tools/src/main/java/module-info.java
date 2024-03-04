185c4197f88596d690dd61cc0d0905e0f6d4670b
Caleb Cushing <xenoterracide@gmail.com>
2024-03-04 18:44:31 -0500

    docs:  mention nullmarked

--- module/tools/src/main/java/module-info.java
+++ module/tools/src/main/java/module-info.java
@@ -2,6 +2,9 @@ import org.jspecify.annotations.NullMarked;
 
 /**
  * Tooling that I believe should be part of the JDK itself.
+ * <p>
+ *   Module is {@link NullMarked} and APIs {@link org.jspecify.annotations.NonNull} unless otherwise documented.
+ * </p>
  */
 @NullMarked module com.xenoterracide.tools.java {
   requires java.base;
