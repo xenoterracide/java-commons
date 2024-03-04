98a6fe6ae7295a7869d5437e01912edd02ab7b84
Caleb Cushing <xenoterracide@gmail.com>
2024-03-04 18:45:15 -0500

    chore:  Nullmarked not needed on package

--- module/tools/src/main/java/com/xenoterracide/tools0/java/function/package-info.java
+++ module/tools/src/main/java/com/xenoterracide/tools0/java/function/package-info.java
@@ -1,7 +1,4 @@
 /**
  * Provides functional utilities for use with {@link java.util.function}.
  */
-@NullMarked
 package com.xenoterracide.tools0.java.function;
-
-import org.jspecify.annotations.NullMarked;
