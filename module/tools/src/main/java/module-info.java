2fa22708650e4e6593bc7db6d8640a9be6e1552a
Caleb Cushing <xenoterracide@gmail.com>
2024-03-11 20:44:17 -0400

    fix: module info

--- module/tools/src/main/java/module-info.java
+++ module/tools/src/main/java/module-info.java
@@ -7,7 +7,7 @@ import org.jspecify.annotations.NullMarked;
  * </p>
  */
 @NullMarked module com.xenoterracide.tools.java {
+  requires transitive static org.jspecify;
   requires java.base;
-  requires org.jspecify;
   exports com.xenoterracide.tools0.java.function;
 }
