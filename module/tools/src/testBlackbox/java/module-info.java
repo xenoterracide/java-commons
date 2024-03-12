24cd4d2d0dc130c54085e5566c29c2370092e286
Caleb Cushing <xenoterracide@gmail.com>
2024-03-11 23:26:16 -0400

    chore: export isn't needed

--- module/tools/src/testBlackbox/java/module-info.java
+++ module/tools/src/testBlackbox/java/module-info.java
@@ -2,6 +2,5 @@ module com.xenoterracide.blackbox {
   requires com.xenoterracide.tools.java;
   requires org.junit.jupiter.api;
   requires org.assertj.core;
-  exports com.xenoterracide.blackbox;
   opens com.xenoterracide.blackbox to org.junit.platform.commons;
 }
