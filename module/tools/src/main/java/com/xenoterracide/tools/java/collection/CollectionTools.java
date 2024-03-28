fad6bb7d9d80181425dbda889e77c5274e00e3f3
Caleb Cushing <xenoterracide@gmail.com>
2024-03-27 21:48:17 -0400

    feat: add CollectionTools#addIf

---
+++ module/tools/src/main/java/com/xenoterracide/tools/java/collection/CollectionTools.java
@@ -0,0 +1,38 @@
+// © Copyright 2024 Caleb Cushing
+// SPDX-License-Identifier: Apache-2.0
+
+package com.xenoterracide.tools.java.collection;
+
+import java.util.Collection;
+import java.util.function.Predicate;
+import org.jspecify.annotations.NonNull;
+import org.jspecify.annotations.Nullable;
+
+/**
+ * Provides utility methods for working with collections.
+ */
+public final class CollectionTools {
+
+  private CollectionTools() {}
+
+  /**
+   * Adds an element to a collection if a condition is met.
+   *
+   * @param collection the collection to add to
+   * @param element the element to add
+   * @param condition the condition to test
+   * @param <T> the type of the element
+   */
+  public static <T> void addIf(
+    @Nullable Collection<? super T> collection,
+    @Nullable T element,
+    @NonNull Predicate<Collection<? super T>> condition
+  ) {
+    if (collection == null) {
+      return;
+    }
+    if (condition.test(collection)) {
+      collection.add(element);
+    }
+  }
+}
