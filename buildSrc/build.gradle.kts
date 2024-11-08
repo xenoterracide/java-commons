5ca366e047672095b906b8afc2fd0167eaa69f69
Caleb Cushing <caleb.cushing@gmail.com>
2024-11-08 11:56:28 -0500

    chore: use conventions

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -11,6 +11,8 @@ dependencyLocking { lockAllConfigurations() }
 
 dependencies {
   implementation(files(libs.javaClass.superclass.protectionDomain.codeSource.location))
+  implementation(libs.plugin.convention.coverage)
+  implementation(libs.plugin.convention.publish)
   implementation(libs.plugin.spotless)
   implementation(libs.plugin.spotbugs)
   implementation(libs.plugin.errorprone)
