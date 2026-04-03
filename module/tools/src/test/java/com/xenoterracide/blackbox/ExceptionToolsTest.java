8a4911bb18029dc48937db92709e596ddb9ca643
Caleb Cushing <caleb.cushing@gmail.com>
2026-04-03 13:36:24 -0400

    chore: update `pom.xml` ErrorProne arguments and replace `Assertions.assertThat` with `assertThat` in `ExceptionToolsTest`

--- module/tools/src/test/java/com/xenoterracide/blackbox/ExceptionToolsTest.java
+++ module/tools/src/test/java/com/xenoterracide/blackbox/ExceptionToolsTest.java
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: Copyright © 2024 Caleb Cushing
+// SPDX-FileCopyrightText: Copyright © 2024, 2026 Caleb Cushing
 //
 // SPDX-License-Identifier: Apache-2.0
 
@@ -11,14 +11,13 @@ import com.xenoterracide.tools.java.function.ExceptionTools;
 import io.vavr.control.Try;
 import java.io.IOException;
 import java.io.UncheckedIOException;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class ExceptionToolsTest {
 
   @Test
   void convertRuntimeExceptionsAreJustRethrown() {
-    Assertions.assertThat(ExceptionTools.toRuntime(new NullPointerException()))
+    assertThat(ExceptionTools.toRuntime(new NullPointerException()))
       .isInstanceOf(NullPointerException.class)
       .hasNoCause();
   }
